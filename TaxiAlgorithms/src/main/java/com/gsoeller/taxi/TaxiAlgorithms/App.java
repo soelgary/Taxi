363ee3b23421321cd92844ee8738f8a3c1607817
Gary Soeller <soeller.g@husky.neu.edu>
2014-07-15 19:11:41 -0400

    algorithms shell

---
+++ TaxiAlgorithms/src/main/java/com/gsoeller/taxi/TaxiAlgorithms/App.java
@@ -0,0 +1,13 @@
+package com.gsoeller.taxi.TaxiAlgorithms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
